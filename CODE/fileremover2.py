2cde4ed57aab5872c99dbdec7d3c47fd143c6e8b
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-06-13 20:30:23 -0400

    Added Darwin function. Hopefully soon the BSDs. Might be similar to Linux and Darwin's /, but I want it personal to each OS, which is why Darwin's uses /System.

--- CODE/fileremover2.py
+++ CODE/fileremover2.py
@@ -6,7 +6,7 @@ import platform
 current_os = platform.system()
 
 
-def programwindows():
+def programWindows():
     home = os.path.expanduser("C:\\Windows\\System32")
 
     while True:
@@ -52,7 +52,53 @@ def programwindows():
             print("INVALID.\n")
 
 
-def programlinux():
+def programLinux():
+    home = os.path.expanduser("/")
+
+    while True:
+        print("\n[F]ILE / [D]IR / [R]EMOVE ROOT / [E TO EXIT]")
+        choice = input("CHOICE: ").upper()
+
+
+        if choice == "F":
+            time.sleep(0.5); print("\nTYPE THE PATH.")
+            file = input("INPUT: ")
+            full_path = os.path.expanduser(file)
+
+            if os.path.exists(full_path): #checks for the file
+                print(f"Success.. Path resolved to: {full_path}\n") #lets the user know the file exists
+                print(f"DELETING: {full_path}") #informing it is deleting [best to check if it is]
+                os.remove(full_path) #deletes the file
+            else:
+                print(f"THE FILE ({full_path}) DOES NOT EXIT") #alerts the user the file does not exist
+
+
+        if choice == "D":
+            time.sleep(0.5); print("\nTYPE THE PATH")
+            directory = input("INPUT: ")
+            full_path = os.path.expanduser(directory)
+
+            if os.path.exists(full_path): #checks for the file
+                print(f"Success.. Path resolved to: {full_path}") #lets the user know the file exists
+                print(f"DELETING: {full_path}\n") #informing it is deleting [best to check if it is]
+                shutil.rmtree(full_path) #deletes the file
+            else:
+                print(f"THE FOLDER ({full_path}) DOES NOT EXIST")
+
+        if choice == "R":
+            time.sleep(0.5); print("\nREMOVING ROOT")
+            shutil.rmtree(home)
+            break
+
+        if choice == "E":
+            print("EXITING")
+            break
+
+        if choice not in ["F", "D", "E"]:
+            print("INVALID.\n")
+
+
+def programMac():
     home = os.path.expanduser("/")
 
     while True:
@@ -101,17 +147,18 @@ def programlinux():
 
 if current_os == "Windows":
     print("WINDOWS MACHINE DETECTED. PROGRAM FUNCTION: 100%")
-    programwindows()
+    programWindows()
 
 elif current_os == "Darwin":
-    print("DARWIN MACHINE DETECTED. PROGRAM WILL NOT FUNCTION.")
+    print("DARWIN MACHINE DETECTED. PROGRAM FUNCTION: 100%")
+    programMac()
 
 elif current_os == "iOS":
     print("iOS MACHINE DETECTED. PROGRAM WILL NOT FUNCTION.")
 
 elif current_os == "Linux":
     print("LINUX MACHINE DETECTED. PROGRAM FUNCTION: 100%")
-    programlinux()
+    programLinux()
 
 if current_os in ["FreeBSD", "OpenBSD", "NetBSD"]:
     print("BSD MACHINE DETECTED. PROGRAM WILL NOT RUN.")
